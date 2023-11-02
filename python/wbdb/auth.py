0c4334678f736bd90b996995e6724d743f9dd645
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2023-11-02 03:19:50 +0100

    Implement require_standname_or_admin_role decorator

--- python/wbdb/auth.py
+++ python/wbdb/auth.py
@@ -1,6 +1,7 @@
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import check_password_hash
 from wbdb import db_handler
+from functools import wraps
 
 #
 # Authentication stuff
@@ -25,8 +26,23 @@ def verify_password(username, password):
 def get_user_roles(user):
     return user["roles"]
 
+
 #
 # Authorization stuff
 #
 
-# TODO
+def require_standname_or_admin_role(f):
+    """The effect of this decorator is requiring login and only authorizing
+    access when the role of the logged-in user matches 'admin' or a kwarg with
+    the key 'standname'.
+    """
+
+    @wraps(f)
+    def dec_func(*args, **kwargs):
+        @auth.login_required(role=[kwargs['standname'], "admin"])
+        def proxy_func():
+            return f(*args, **kwargs)
+
+        return proxy_func()
+
+    return dec_func
