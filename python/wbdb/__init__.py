28dead40fab0e555b6c6559aeeabf76477a76adf
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2023-11-06 23:23:57 +0000

    Enable CORS

--- python/wbdb/__init__.py
+++ python/wbdb/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from wbdb.DBHandler import DBHandler
 from wbdb.loggers import db_logger
 
 app = Flask(__name__)
+CORS(app)
 db_handler = DBHandler(db_logger)
 
 import wbdb.routes
